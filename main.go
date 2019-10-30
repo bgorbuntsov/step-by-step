28636f77906d201394cb83231d5b4afac9835817
Boris <gorbuntsov@gmail.com>
2019-10-30 13:01:34 +0300

    Glide installed. Dockerfile created. Code extended to port choosing via sys variable

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/Sirupsen/logrus"
 	"github.com/takama/router"
+    "os"
 )
 
 var log = logrus.New()
@@ -10,8 +11,10 @@ var log = logrus.New()
 // Run server: go build && step-by-step
 // Try requests: curl http://127.0.0.1:8000/test
 func main() {
+    port := os.Getenv("SERVICE_PORT")
+
 	r := router.New()
 	r.Logger = logger
 	r.GET("/", home)
-	r.Listen(":8000")
+	r.Listen(":" + port)
 }
